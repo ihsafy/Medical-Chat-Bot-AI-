204784c5c28f4da0e88cb8ebbef17bf97cebf62d
ihsafy <ihsafy2k21@gmail.com>
2024-12-25 22:18:21 +0600

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Medical-Chat-Bot-AI-
+conda create -n medibot python=3.10 -y
+pip install -r requirements.txt
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bat Man',
+    author_email= 'ihsafy2k21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
